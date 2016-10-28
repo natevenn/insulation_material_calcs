0ecefc0dd4f79cde24d7a8631dffb52982235d85
Nathan Venn <natevenn@gmail.com>
2016-10-27 21:55:38 -0600

    create function isOdd to determine if to add 1 ft or 2 ft to over all length

--- wall_length_calcs.js
+++ wall_length_calcs.js
@@ -1,28 +1,13 @@
 function calcWallLengthRolls(len) {
-  len = parseInt(len)
-  lenPlusFoot = len + 1
+  isOdd(len) ? len += 1 : len += 2
   var starter_roll = 0
   var remainder = 1
-  var maxRemainder = 1
-  while(remainder !== 0) {
-    if(remainder < 0) { break; }
-    starter_roll += 4
-    remainder = (lenPlusFoot - starter_roll) % 6
-    var currentRemainder = (len - starter_roll) % 6
-    if (currentRemainder > maxRemainder) {
-      maxRemainder = currentRemainder
-    }
-  }
 
-  if(remainder < 0) {
-   starter_roll = 0
-   while(remainder !== maxRemainder) {
+  while(remainder !== 0) {
     starter_roll += 4
     remainder = (len - starter_roll) % 6
-   }
- }
-
-  if(remainder === 0) { len = lenPlusFoot }
+    var currentRemainder = (len - starter_roll) % 6
+  }
 
   var fourFooters = starter_roll / 4
   var sixFooters = (len - starter_roll) / 6
@@ -32,3 +17,8 @@ function calcWallLengthRolls(len) {
     sixFooters: sixFooters
   }
 }
+
+function isOdd(num) {
+  if(num % 2 !== 0) { return true }
+}
+
